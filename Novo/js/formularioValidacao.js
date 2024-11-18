955b1192f70c56165dd00ea2a3913406d4686d66
Thiago Telma <thiago04felipe@gmail.com>
2024-11-18 15:43:41 -0300

    Alterações finais no projeto.

--- Novo/js/formularioValidacao.js
+++ Novo/js/formularioValidacao.js
@@ -4,73 +4,81 @@ const form = document.getElementById("form");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const phone = document.getElementById("phone");
+const text = document.getElementById("texto");
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  checkInputs();
+    checkInputs();
 });
 
 function checkInputs() {
-  const usernameValue = username.value;
-  const emailValue = email.value;
-  const phoneValue = phone.value;
+    const usernameValue = username.value;
+    const emailValue = email.value;
+    const phoneValue = phone.value;
+    const textValue = text.value;
 
-  if (usernameValue === "") {
-    setErrorFor(username, "O nome de usuário é obrigatório.");
-  } else {
-    setSuccessFor(username);
-  }
+    if (usernameValue === "") {
+        setErrorFor(username, "O nome de usuário é obrigatório.");
+    } else {
+        setSuccessFor(username);
+    }
 
-  if (emailValue === "") {
-    setErrorFor(email, "O email é obrigatório.");
-  } else if (!checkEmail(emailValue)) {
-    setErrorFor(email, "Por favor, insira um email válido.");
-  } else {
-    setSuccessFor(email);
-  }
+    if (emailValue === "") {
+        setErrorFor(email, "O email é obrigatório.");
+    } else if (!checkEmail(emailValue)) {
+        setErrorFor(email, "Por favor, insira um email válido.");
+    } else {
+        setSuccessFor(email);
+    }
 
-  if (phoneValue === "") {
-    setErrorFor(phone, "O telefone é obrigatório.");
-  } else {
-    setSuccessFor(phone);
-  }
+    if (phoneValue === "") {
+        setErrorFor(phone, "O telefone é obrigatório.");
+    } else {
+        setSuccessFor(phone);
+    }
 
-  const formControls = form.querySelectorAll(".form-control");
+    if (textValue === "") {
+        setErrorFor(text, "Digite algo para a nossa melhoria.");
+    } else {
+        setSuccessFor(text);
+    }
 
-  const formIsValid = [...formControls].every((formControl) => {
-    return formControl.className === "form-control success";
-  });
+    const formControls = form.querySelectorAll(".form-control");
 
-  if (formIsValid) {
-    alert("Inscrição concluída!");
-    form.reset();
-    formControls.forEach((formControl) => {
-      formControl.className = "form-control invisible";
+    const formIsValid = [...formControls].every((formControl) => {
+        return formControl.className === "form-control success";
     });
-  }
+
+    if (formIsValid) {
+        alert("Inscrição concluída!");
+        form.reset();
+        formControls.forEach((formControl) => {
+            formControl.className = "form-control invisible";
+        });
+    }
 }
 
 function setErrorFor(input, message) {
-  const formControl = input.parentElement;
-  const small = formControl.querySelector("small");
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small");
 
-  // Adiciona a mensagem de erro
-  small.innerText = message;
+    // Adiciona a mensagem de erro
+    small.innerText = message;
 
-  // Adiciona a classe de erro
-  formControl.className = "form-control error";
+    // Adiciona a classe de erro
+    formControl.className = "form-control error";
 }
 
 function setSuccessFor(input) {
-  const formControl = input.parentElement;
+    const formControl = input.parentElement;
 
-  // Adicionar a classe de sucesso
-  formControl.className = "form-control success";
+    // Adicionar a classe de sucesso
+    formControl.className = "form-control success";
 }
 
 function checkEmail(email) {
-  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-    email
-  );
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email
+    );
 }
